0934648cccc27b0473459e9188e59fea08cedbd2
Asaad Jaber <assaadmjaber@icloud.com>
2024-01-05 19:43:08 +0200

    Sample wallet using Locale

--- SwiftUI-Data/SwiftUI-Data/Environment/EView.swift
+++ SwiftUI-Data/SwiftUI-Data/Environment/EView.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct EView: View {
+    @Environment(\.locale) var locale: Locale
+    @State private var selectedLocale: LocaleSelection = .englishUS
+    
+    var wallet: Wallet = Wallet(balance: 14, locale: Locale.current)
+    
+    var body: some View {
+        List {
+            Section("Sample Wallet") {
+                HStack {
+                    Text("Date")
+                    Spacer()
+                    Text(wallet.getDate(locale: selectedLocale.locale))
+                }
+                HStack {
+                    Text("Balance")
+                    Spacer()
+                    Text(wallet.getBalance(locale: selectedLocale.locale))
+                }
+            }
+            Section("Locale") {
+                Picker("Locale", selection: $selectedLocale) {
+                    Text("Arabic").tag(LocaleSelection.arabic)
+                    Text("English (U.S.)").tag(LocaleSelection.englishUS)
+                    Text("English (U.K.)").tag(LocaleSelection.englishUK)
+                }
+            }
+        }.listStyle(.insetGrouped)
+    }
+}
+
+enum LocaleSelection: String, CaseIterable, Identifiable {
+    case arabic, englishUS, englishUK
+    var name: String {
+        switch self {
+        case .arabic:
+            "Arabic"
+        case .englishUS:
+            "English (U.S.)"
+        case .englishUK:
+            "English (UK)"
+        }
+    }
+    var id: String {
+        switch self {
+        case .arabic:
+            "ar"
+        case .englishUS:
+            "en-US"
+        case .englishUK:
+            "en-UK"
+        }
+    }
+    var locale: Locale {
+        Locale(identifier: self.id)
+    }
+}
+
+#Preview {
+    EView()
+}
--- SwiftUI-Data/SwiftUI-Data/Environment/Wallet.swift
+++ SwiftUI-Data/SwiftUI-Data/Environment/Wallet.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class Wallet {
+    var balance: Int
+    var locale: Locale
+    
+    init(balance: Int, locale: Locale) {
+        self.balance = balance
+        self.locale = locale
+    }
+    
+    func getDate(locale: Locale) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        let date = Date(timeIntervalSinceReferenceDate: Date.now.timeIntervalSinceReferenceDate)
+        
+        dateFormatter.locale = locale
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    func getBalance(locale: Locale) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.locale = locale
+        numberFormatter.numberStyle = .currency
+        numberFormatter.maximumFractionDigits = 0
+        
+        let number = numberFormatter.string(from: NSNumber(integerLiteral: balance))
+
+        return number!
+    }
+}
